5390d596568e914da244fc5c55f91436390a94e5
Yuliia Lysak <julia.m.lysak@gmail.com>
2022-07-04 23:49:27 +0300

    Linear Equations Solver - stage 4/5: When things get complicated

--- linear-equations-solver/src/main/java/edu/lysak/solver/Main.java
+++ linear-equations-solver/src/main/java/edu/lysak/solver/Main.java
@@ -1,7 +1,12 @@
 package edu.lysak.solver;
 
+import edu.lysak.solver.v2.Application;
+
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Scanner;
@@ -11,7 +16,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String inputFilePath = Utils.getInputFile(args);
         String outputFilePath = Utils.getOutputFile(args);
+//        solveV1(inputFilePath, outputFilePath);
+        solveV2(inputFilePath, outputFilePath);
+    }
 
+    private static void solveV1(String inputFilePath, String outputFilePath) throws IOException {
         Scanner scanner = new Scanner(Path.of(inputFilePath));
         LinearEquationParser linearEquationParser = new LinearEquationParser();
         LinearEquation linearEquation = linearEquationParser.parse(scanner);
@@ -29,6 +38,14 @@ public class Main {
 
             System.out.printf("The solution is: (%s)%n", stringJoiner);
             System.out.printf("Saved to file %s%n", outputFilePath);
+        }
+    }
+
+    private static void solveV2(String inputFilePath, String outputFilePath) throws FileNotFoundException {
+        try (var input = new Scanner(new File(inputFilePath));
+             var output = new PrintWriter(outputFilePath)
+        ) {
+            new Application(input, output).run();
         }
     }
 }
---
+++ linear-equations-solver/src/main/java/edu/lysak/solver/v2/Application.java
@@ -0,0 +1,26 @@
+package edu.lysak.solver.v2;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.stream.DoubleStream;
+
+public class Application implements Runnable {
+    private final Scanner scanner;
+    private final PrintWriter writer;
+
+    public Application(Scanner scanner, PrintWriter writer) {
+        this.scanner = scanner;
+        this.writer = writer;
+    }
+
+    public void run() {
+        final int cols = scanner.nextInt() + 1;
+        final int rows = scanner.nextInt();
+        final var cells = DoubleStream.generate(scanner::nextDouble)
+                .limit(rows * cols)
+                .toArray();
+
+        final var equation = new LinearEquation(rows, cols, cells);
+        writer.print(equation.getSolution());
+    }
+}
