1ab62c86a5d6166d940d0a1e91041acae2568152
Yuliia Lysak <julia.m.lysak@gmail.com>
2022-09-24 11:27:52 +0300

    Simple Calculator - Stage 2/4: Let's calculate

--- cli-bash/simple-calculator/src/main/resources/simple-calculator.sh
+++ cli-bash/simple-calculator/src/main/resources/simple-calculator.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
-echo "Enter an arithmetic operation:"
-#read input
-#re='^[-0-9]+ [-,+,*,/] [-0-9]+$'
-#if [[ "$input" =~ $re ]]; then
-#    echo "Operation check passed!"
-#else
-#    echo "Operation check failed!"
-#fi
+#calculate() {
+#  if [ "$operator" = "+" ]; then
+#    arithmetic_result=$((number1 + number2))
+#  elif [ "$operator" = "-" ]; then
+#    arithmetic_result=$((number1 - number2))
+#  elif [ "$operator" = "*" ]; then
+#    arithmetic_result=$((number1 * number2))
+#  elif [ "$operator" = "/" ]; then
+#    arithmetic_result=$((number1 / number2))
+#  fi
+#}
 
+echo "Enter an arithmetic operation:"
 read number1 operator number2
 number_regex='^-?[-0-9]+$'
 operator_regex='^[-+*/]$'
 if [[ "$number1" =~ $number_regex ]] && [[ "$number2" =~ $number_regex ]] && [[ "$operator" =~ $operator_regex ]]; then
-    echo "Operation check passed!"
+#  calculate
+  arithmetic_result=$(( $number1 $operator $number2))
+  printf "%s\n" "$arithmetic_result"
 else
-    echo "Operation check failed!"
+  echo "Operation check failed!"
 fi
