3e3002cd72ca6881b2b04361c43de670ea84bd92
Yuliia Lysak <julia.m.lysak@gmail.com>
2022-10-04 23:20:06 +0300

    Enigma - Stage 3/5: Caesar cipher

--- cli-bash/enigma/src/main/resources/enigma.sh
+++ cli-bash/enigma/src/main/resources/enigma.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+constant_key=3
+message_regex='^[A-Z ]+$'
+
 stage_1() {
   echo "Enter a message:"
   read -r input
-  regex='^[A-Z ]+$'
-  if [[ $input =~ $regex ]]; then
+  if [[ $input =~ $message_regex ]]; then
     echo "This is a valid message!"
   else
     echo "This is not a valid message!"
@@ -42,5 +44,105 @@ chr() {
   printf "%b\n" "$(printf "\\%03o" "$1")"
 }
 
+stage_3() {
+  echo "Type 'e' to encrypt, 'd' to decrypt a message:"
+  echo "Enter a command:"
+  read -r command
+  case $command in
+  "e")
+    process_input
+    encrypt "$message"
+    ;;
+  "d")
+    process_input
+    decrypt "$message"
+    ;;
+  *)
+    echo "Invalid command!"
+    ;;
+  esac
+}
+
+process_input() {
+  echo "Enter a message:"
+  read -r message
+  is_valid_message=$(check_message "$message")
+  if ! $is_valid_message; then
+    echo "This is not a valid message!"
+    exit
+  fi
+}
+
+check_message() {
+  if [[ $1 =~ $message_regex ]]; then
+    echo "true"
+  else
+    echo "false"
+  fi
+}
+
+encrypt() {
+  # Convert string to array
+  message_length=${#1}
+  for ((i = 0; i < message_length; i++)); do
+    array+=("${1:i:1}") ## use array indexing for individual chars
+  done
+
+  array_length="${#array[@]}"
+  for ((i = 0; i < array_length; i++)); do
+    value=$(ord "${array[i]}")
+    # check for space (' ' = 32)
+    if [[ $value -eq 32 ]]; then
+      shifted_value=$value
+    else
+      shifted_value=$((value + constant_key))
+    fi
+
+    # start with A after Z
+    if [[ $shifted_value -gt 90 ]]; then
+      shifted_value=$((shifted_value - 26))
+    fi
+
+    encrypted_letter=$(chr "$shifted_value")
+    result+=$encrypted_letter
+    # TODO: uncomment next line for check at hyperskill.org
+    #echo ""
+  done
+  echo "Encrypted message:"
+  echo "$result"
+}
+
+decrypt() {
+  # Convert string to array
+  message_length=${#1}
+  for ((i = 0; i < message_length; i++)); do
+    array+=("${1:i:1}") ## use array indexing for individual chars
+  done
+
+  array_length="${#array[@]}"
+  for ((i = 0; i < array_length; i++)); do
+    value=$(ord "${array[i]}")
+    # check for space (' ' = 32)
+    if [[ $value -eq 32 ]]; then
+      shifted_value=$value
+    else
+      shifted_value=$((value - constant_key))
+    fi
+
+    # start with Z after A
+    if [[ $shifted_value -lt 65 ]] && [[ $shifted_value -ne 32 ]]; then
+      shifted_value=$((shifted_value + 26))
+    fi
+
+    decrypted_letter=$(chr "$shifted_value")
+    result+=$decrypted_letter
+    # TODO: uncomment next line for check at hyperskill.org
+    #echo ""
+  done
+  echo "Decrypted message:"
+  echo "$result"
+}
+
 #stage_1
-stage_2
+#stage_2
+stage_3
