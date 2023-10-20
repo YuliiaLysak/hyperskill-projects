5fbb2d677d6d3ac6dd2e3e4645a599dc7e481f56
Yuliia Lysak <julia.m.lysak@gmail.com>
2023-10-20 22:50:47 +0300

    True or False - Stage 2/6: Access granted

--- cli-bash/true-false/src/main/resources/true-false.sh
+++ cli-bash/true-false/src/main/resources/true-false.sh
@@ -7,3 +7,12 @@ curl --request GET -sL \
      --output 'ID_card.txt'
 
 cat 'ID_card.txt'
+
+curl --request GET -sL \
+     --url 'http://127.0.0.1:8000/login'\
+     --silent \
+     --output 'cookie.txt' \
+     --cookie-jar "$(cat cookie.txt)" \
+     --user "$(cat ID_card.txt)"
+
+echo "Login message: $?"
